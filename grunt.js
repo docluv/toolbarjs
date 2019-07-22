544ab768d006bfe128df7256e4a827856890846f
docluv <chris@love2dev.com>
2019-07-22 10:35:41 -0400

    removed old VS artifacts moved source code around to be more modern not sure the grunt will work right now...

--- grunt.js
+++ grunt.js
@@ -1,9 +1,9 @@
-module.exports = function (grunt) {
+module.exports = function ( grunt ) {
 
     // Project configuration.
-    grunt.initConfig({
+    grunt.initConfig( {
         lint: {
-            all: ['grunt.js', 'js/debug/*.js']
+            all: [ 'grunt.js', 'js/debug/*.js' ]
         },
         jshint: {
             options: {
@@ -13,32 +13,33 @@ module.exports = function (grunt) {
         min: {
             helper: {
                 src: [
-                    'js/debug/helper.js',
-                    'js/debug/helper.extensions.js'
-                    ],
-                dest: 'helper.min.js'
+                    'demo/js/debug/helper.js',
+                    'demo/js/debug/helper.extensions.js'
+                ],
+                dest: 'dist/helper.min.js'
             },
 
             dist: {
                 src: [
-                    'js/debug/toolbar.js'],
-                dest: 'toolbar.min.js'
+                    'demo/js/debug/toolbar.js'
+                ],
+                dest: 'dist/toolbar.min.js'
             }
         },
         concat: {
             license: {
-                src: ['js/debug/license.txt',
-                'js/deep-tissue.min.js'
+                src: [ 'js/debug/license.txt',
+                    'js/deep-tissue.min.js'
                 ],
                 dest: 'js/deep-tissue.min.js',
                 separator: ';'
             }
 
         }
-    });
+    } );
 
 
     // Default task.
-    grunt.registerTask('default', [/*"lint",*/'min']);
+    grunt.registerTask( 'default', [ /*"lint",*/ 'min' ] );
 
 };
